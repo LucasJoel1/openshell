edc079176bbbb56575061709054f9e4de892b372
Lucas Joel <81326116+LucasJoel1@users.noreply.github.com>
2022-10-28 15:33:15 +0000

    modified:   .vscode/settings.json 	modified:   a.out 	new file:   hello.txt 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -11,12 +11,29 @@
 
 using namespace std;
 
+
+
+
 int edit() {
     unsigned char buffer[16];
-    cout << "Enter Some Text";
+    int sizeOfBuffer = 0;
+    cout << "Enter Some Text: ";
     cin >> buffer;
-    int fd = open("./hello.txt", O_RDONLY);
-    read(fd, buffer, sizeof(buffer));
+    for (int i = 0; buffer[i] != 0; i++) {
+        sizeOfBuffer++;
+    }
+    unsigned char temp[sizeOfBuffer];
+    for (int i = 0; i < sizeOfBuffer; i++) {
+        temp[i] = buffer[i];
+    }
+    int fd = open("./hello.txt", O_RDWR);
+    read(fd, buffer, sizeof(temp));
+    write(fd, temp, sizeof(temp));
+    int fsync(int fd);
+    return 0;
+}
+
+int read() {
     return 0;
 }
 
@@ -37,42 +54,41 @@ void mkd() {
 
 }
 
-vector<string> parseInput(string input) {
-    vector<string> result;
-    string command;
-    string temp;
-    command = input.substr(0, input.find("-"));
-    vector<string> parameters;
-    for (int i = 1; i < input.length(); i++) {
-        if (input[i] == '-') {
-            for (int j = i + 1; 1 != 2; i++) {
-                if (!input[j] == '-') {
-                    temp = temp + input[j];
-                } else {
-                    parameters.push_back(temp);
-                    temp.clear();
-                    break;
-                }
-            }
-    }
-    return result;
-}
+// vector<string> parseInput(string input) {
+//     vector<string> result;
+//     string command;
+//     string temp;
+//     command = input.substr(0, input.find("-"));
+//     vector<string> parameters;
+//     for (int i = 1; i < input.length(); i++) {
+//         if (input[i] == '-') {
+//             for (int j = i + 1; 1 != 2; i++) {
+//                 if (!input[j] == '-') {
+//                     temp = temp + input[j];
+//                 } else {
+//                     parameters.push_back(temp);
+//                     temp.clear();
+//                     break;
+//                 }
+//             }
+//     }
+//     return result;
+// }
 
 
 int main() {
     string option;
     cout << "what do you want to do: ";
     getline(cin, option);
-    cout << option;
-    vector<string> command;
-    if (option.find("-")) {
-        command = parseInput(option);
-    } else {
-        command.push_back(option);
-    }
-    for(int i = 0; i < command.size(); i++) {
-        cout << command[i] << endl;
-    }
+    // vector<string> command;
+    // if (option.find("-")) {
+    //     command = parseInput(option);
+    // } else {
+    //     command.push_back(option);
+    // }
+    // for(int i = 0; i < command.size(); i++) {
+    //     cout << command[i] << endl;
+    // }
 
     if (option == "edit") {
         edit();
