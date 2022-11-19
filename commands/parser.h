74160cc487c9dc48fb0e5a1830ea9add530fec18
LucasJoel1 <ljoel1@outlook.com>
2022-11-18 22:31:35 -0500

    deleted:    commands/parser 	modified:   commands/parser.h 	modified:   openshell

--- commands/parser.h
+++ commands/parser.h
@@ -2,11 +2,13 @@
 
 void parse(string toBeParsed) {
     vector<string> parsedVector;
-    regex e("(-[a-z]+ ?[^ ]+)+");
-    sregex_token_iterator iter(toBeParsed.begin(), toBeParsed.end(), e, -1);
+    regex e("-([\\w]+)+ ([\\w]+)+");
+    sregex_token_iterator iter(toBeParsed.begin(), toBeParsed.end(), e, 0);
     sregex_token_iterator end;
     for(; iter != end; ++iter) {
         parsedVector.push_back(*iter);
-        cout << *iter << endl;
+    }
+    for(int i = 0; i < parsedVector.size(); i++) {
+        cout << parsedVector[i] << endl;
     }
 }
